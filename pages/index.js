28d7e5f72f2886da6e732163d30f66688a83f48d
pinocchio-life-like <icbr19fl@gmail.com>
2023-09-22 17:51:56 +0300

    blog

--- pages/index.js
+++ pages/index.js
@@ -20,35 +20,33 @@ export default function Home({ blogs, topics }) {
   return (
     <>
       <Head>
-        <title>Bits-0f-C0de 🚀</title>
-        <meta name="title" content="Bits-0f-C0de 🚀" />
+        <title>eliyasblog 🚀</title>
+        <meta name="title" content="eliyasblog 🚀" />
         <meta
           name="description"
           content="Tech blogs and articles on various topics related to Software Development"
         />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://blogs.soumya-jit.tech/" />
-        <meta property="og:title" content="Bits-0f-C0de 🚀" />
+        <meta property="og:title" content="eliyasblog 🚀" />
         <meta
           property="og:description"
           content="Tech blogs and articles on various topics related to Software Development"
         />
         <meta
           property="og:image"
-          content="https://raw.githubusercontent.com/pinocchio-life-like/Bits-0f-C0de/main/Extra/sc.png"
+          content="https://raw.githubusercontent.com/pinocchio-life-like/eliyas_blog/main/Extra/sc.png"
         />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://blogs.soumya-jit.tech/" />
-        <meta property="twitter:title" content="Bits-0f-C0de 🚀" />
+        <meta property="twitter:title" content="eliyasblog 🚀" />
         <meta
           property="twitter:description"
           content="Tech blogs and articles on various topics related to Software Development"
         />
         <meta
           property="twitter:image"
-          content="https://raw.githubusercontent.com/pinocchio-life-like/Bits-0f-C0de/main/Extra/sc.png"
+          content="https://raw.githubusercontent.com/pinocchio-life-like/eliyas_blog/main/Extra/sc.png"
         />
       </Head>
 
